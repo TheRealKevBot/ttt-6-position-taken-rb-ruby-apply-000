09d9087e5af9c74335936385face87501417da5c
Kevin Glasgow <kglas001@gmail.com>
2020-08-21 18:10:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,21 +1,12 @@
 # code your #position_taken? method here!
 
 
-def position_taken?(board, index) 
-  if board[index] == ""
-    return false 
-  elsif board[index] == " "
-    return false 
-  elsif board[index] == nil 
-    return false 
-  else return true
-  end 
-end 
+
 
 #or
 
 def position_taken?(board, index)
-  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil) 
     false : true
   end 
 end
