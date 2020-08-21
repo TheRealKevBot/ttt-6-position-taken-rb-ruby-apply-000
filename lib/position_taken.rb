b57904e1383864e91250d87553081ba5c262be9e
Kevin Glasgow <kglas001@gmail.com>
2020-08-21 18:09:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -12,5 +12,12 @@ def position_taken?(board, index)
   end 
 end 
 
-#or 
+#or
 
+def position_taken?(board, position)
+  (board[position] ==  " " || board[position] == "" || board[position] == nil) ? 
+
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+  end 
+end
